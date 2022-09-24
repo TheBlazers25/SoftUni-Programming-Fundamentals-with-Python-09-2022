982f1e16ae880d6a6e4f635811ee658de47969dc
TheBlazers25 <theblazers25@gmail.com>
2022-09-24 18:36:13 +0300

    Update pound_to_dollars_additional_example.py

--- Exercises/Data Types and Variables/pound_to_dollars_additional_example.py
+++ Exercises/Data Types and Variables/pound_to_dollars_additional_example.py
@@ -4,4 +4,4 @@ amount = int(input('Amount in GBP: '))
 c = CurrencyRates()
 current_rate = c.get_rate('GBP', 'USD')
 result = amount * current_rate
-print(f'{amoun} GBP is {result:.3f} USD')
+print(f'{amount} GBP is {result:.3f} USD')
