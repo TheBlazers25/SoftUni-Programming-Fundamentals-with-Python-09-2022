93c088e62e640c3a20bfd862fca067010b27bfaf
TheBlazers25 <theblazers25@gmail.com>
2022-09-25 18:26:03 +0300

    Removing unnecessary lines

--- Exercises/Data Types and Variables/integer_operations.py
+++ Exercises/Data Types and Variables/integer_operations.py
@@ -2,6 +2,5 @@ first_number = int(input())
 second_number = int(input())
 third_number = int(input())
 fourth_number = int(input())
-sum = ((first_number + second_number) / third_number)
-final_sum = int(sum) * fourth_number
-print(int(final_sum))
+final_sum = (first_number + second_number) // third_number * fourth_number
+print(final_sum)
--- Exercises/Data Types and Variables/sum_of_chars.py
+++ Exercises/Data Types and Variables/sum_of_chars.py
@@ -4,9 +4,3 @@ for lines in range(number_of_lines):
     letter = str(input())
     counter  += ord(letter)
 print(f"The sum equals: {counter}")
-
-# name = input("Enter your name: ")
-# for c in name:
-#   print("The ASCII value for ", c, "is", ord(c))
-# print("The total sum is: ",sum([ord(c) for c in name]))
-
